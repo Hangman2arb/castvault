bd51808810d90eadf8844a8efc67f20342ca3f04
AdrianPushTech <adrian.rojas@pushtech.com>
2024-03-19 08:09:10 +0100

    Fixed off canvas functionality and styles.

--- app/components/slider_component.rb
+++ app/components/slider_component.rb
@@ -1,5 +1,5 @@
 class SliderComponent < ViewComponent::Base
-  def initialize(selected_val_min: 0, val_min: 0, key_min:, selected_val_max: 100, val_max: 100, key_max:, form:)
+  def initialize(selected_val_min: 0, val_min: 0, key_min:, selected_val_max: 100, val_max: 100, key_max:, form:, label: '')
     @val_min = val_min
     @selected_val_min = selected_val_min
     @key_min = key_min
@@ -7,5 +7,6 @@ class SliderComponent < ViewComponent::Base
     @selected_val_max = selected_val_max
     @key_max = key_max
     @form = form
+    @label = label
   end
 end
--- app/javascript/controllers/profiles_form_controller.js
+++ app/javascript/controllers/profiles_form_controller.js
@@ -7,14 +7,25 @@ export default class extends Controller {
                     "globalTab", "habilitiesTab", "profileModal", "profileOverlay"]
 
   open() {
-    this.modalTarget.classList.remove("hidden")
-    this.overlayTarget.classList.remove("hidden")
+    this.overlayTarget.classList.remove("hidden");
+    // Usar setTimeout para asegurar que la clase se añade después de que el elemento se vuelve visible.
+    setTimeout(() => {
+      this.modalTarget.classList.remove("translate-x-full");
+      this.modalTarget.classList.add("translate-x-0");
+    }, 10);
   }
 
   close() {
-    this.modalTarget.classList.add("hidden")
-    this.overlayTarget.classList.add("hidden")
+    this.modalTarget.classList.remove("translate-x-0");
+    this.modalTarget.classList.add("translate-x-full");
+
+    // Esperar a que termine la transición antes de ocultar el overlay
+    // La duración debe coincidir con la duración de la transición del panel (1000ms en este caso)
+    setTimeout(() => {
+      this.overlayTarget.classList.add("hidden");
+    }, 1000);  // Asegúrate de que este tiempo coincida con la duración de la transición en tu CSS
   }
+
 
   closeProfileModal() {
     this.profileModalTarget.classList.add("hidden")
@@ -25,16 +36,22 @@ export default class extends Controller {
     event.preventDefault()
     this.globalTabContentTarget.classList.remove("hidden")
     this.habilitiesTabContentTarget.classList.add("hidden")
-    this.globalTabTarget.classList.add("text-indigo-400")
-    this.habilitiesTabTarget.classList.remove("text-indigo-400")
+    this.globalTabTarget.classList.add("bg-gray-200")
+    this.globalTabTarget.classList.remove("text-gray-200")
+    this.habilitiesTabTarget.classList.remove("bg-gray-200")
+    this.habilitiesTabTarget.classList.remove("text-gray-800")
+    this.habilitiesTabTarget.classList.add("text-gray-200")
   }
 
   switchToHabilities(event) {
     event.preventDefault()
     this.globalTabContentTarget.classList.add("hidden")
     this.habilitiesTabContentTarget.classList.remove("hidden")
-    this.habilitiesTabTarget.classList.add("text-indigo-400")
-    this.globalTabTarget.classList.remove("text-indigo-400")
+    this.habilitiesTabTarget.classList.add("bg-gray-200")
+    this.habilitiesTabTarget.classList.remove("text-gray-200")
+    this.globalTabTarget.classList.remove("bg-gray-200")
+    this.globalTabTarget.classList.remove("text-gray-800")
+    this.globalTabTarget.classList.add("text-gray-200")
   }
 
   openModal(event) {
