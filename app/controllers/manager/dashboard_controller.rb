530660a901f1d2c3df605889ae9eb8bb2782de61
AdrianPushTech <adrian.rojas@pushtech.com>
2024-04-02 07:03:35 +0200

    Refactor of dashboard code and translations.

--- app/controllers/manager/dashboard_controller.rb
+++ app/controllers/manager/dashboard_controller.rb
@@ -15,21 +15,22 @@ class Manager::DashboardController < ApplicationController
     @form_with_less_profiles = @forms_ordered_by_use.last
 
     @profiles_by_date = @profiles.group_by_day(:created_at).count
-    @profiles_by_ethnicity = @profiles.group(:ethnicity).count.transform_keys do |key|
+    @profiles_by_ethnicity = @profiles.group(:ethnicity).count.reject { |key, _| key.blank? }.transform_keys do |key|
       I18n.t("ethnicities.#{key}", default: key.to_s.humanize)
     end
-    @profiles_by_gender = @profiles.group(:gender).count.transform_keys do |key|
+    @profiles_by_gender = @profiles.group(:gender).count.reject { |key, _| key.blank? }.transform_keys do |key|
       I18n.t("genders.#{key}", default: key.to_s.humanize)
     end
-    @hair_type_distribution = @profiles.group(:hair_type).count.transform_keys do |key|
+    @hair_type_distribution = @profiles.group(:hair_type).count.reject { |key, _| key.blank? }.transform_keys do |key|
       I18n.t("hair_types.#{key}", default: key.to_s.humanize)
     end
-    @hair_color_distribution = @profiles.group(:hair_color).count.transform_keys do |key|
+    @hair_color_distribution = @profiles.group(:hair_color).count.reject { |key, _| key.blank? }.transform_keys do |key|
       I18n.t("hair_colors.#{key}", default: key.to_s.humanize)
     end
-    @eye_color_distribution = @profiles.group(:eye_color).count.transform_keys do |key|
+    @eye_color_distribution = @profiles.group(:eye_color).count.reject { |key, _| key.blank? }.transform_keys do |key|
       I18n.t("eye_colors.#{key}", default: key.to_s.capitalize)
     end
+
 
     @agency_distribution = @profiles.group(:agency).count.transform_keys do |key|
       key ? I18n.t('labels.yes') : I18n.t('labels.no')
@@ -44,16 +45,24 @@ class Manager::DashboardController < ApplicationController
     end
 
     @charts = [
-      { chart_type: 'line_chart', data: @profiles_by_date, key: 'profiles_by_date', options: { legend: false } },
-      { chart_type: 'pie_chart', data: @profiles_by_ethnicity, key: 'profiles_by_ethnicity', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } },
-      { chart_type: 'pie_chart', data: @profiles_by_gender, key: 'profiles_by_gender', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } },
-      { chart_type: 'pie_chart', data: @hair_type_distribution, key: 'hair_type', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } },
-      { chart_type: 'pie_chart', data: @hair_color_distribution, key: 'hair_color', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } },
-      { chart_type: 'bar_chart', data: @eye_color_distribution, key: 'eye_color', options: {} },
-      { chart_type: 'pie_chart', data: @agency_distribution, key: 'agency', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } },
-      { chart_type: 'pie_chart', data: @passport_distribution, key: 'passport', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } },
-      { chart_type: 'pie_chart', data: @driving_license_distribution, key: 'driving_license', options: { donut: true, colors: chart_colors, dataset: { borderColor: '#111827', borderWidth: 5 }, legend: false } }
+      # Crecimiento de Perfiles
+      { chart_type: 'line_chart', data: @profiles_by_date, key: 'profiles_by_date', options: line_chart_options, section: t('dashboard_controller.sections.profile_growth') },
+
+      # Demografía
+      { chart_type: 'bar_chart', data: @profiles_by_ethnicity, key: 'profiles_by_ethnicity', options: bar_options, section: t('dashboard_controller.sections.demographics') },
+      { chart_type: 'pie_chart', data: @profiles_by_gender, key: 'profiles_by_gender', options: pie_chart_options, section: t('dashboard_controller.sections.demographics') },
+
+      # Distribución de Características Físicas
+      { chart_type: 'bar_chart', data: @hair_type_distribution, key: 'hair_type', options: bar_options, section: t('dashboard_controller.sections.physical_features') },
+      { chart_type: 'bar_chart', data: @hair_color_distribution, key: 'hair_color', options: bar_options, section: t('dashboard_controller.sections.physical_features') },
+      { chart_type: 'bar_chart', data: @eye_color_distribution, key: 'eye_color', options: bar_options, section: t('dashboard_controller.sections.physical_features') },
+
+      # Información de Agencia y Documentación
+      { chart_type: 'pie_chart', data: @agency_distribution, key: 'agency', options: pie_chart_options, section: t('dashboard_controller.sections.agency_and_documentation') },
+      { chart_type: 'pie_chart', data: @passport_distribution, key: 'passport', options: pie_chart_options, section: t('dashboard_controller.sections.agency_and_documentation') },
+      { chart_type: 'pie_chart', data: @driving_license_distribution, key: 'driving_license', options: pie_chart_options, section: t('dashboard_controller.sections.agency_and_documentation') }
     ]
+
 
     @cards = [
       ['total_profiles', @total_profiles],
@@ -67,7 +76,23 @@ class Manager::DashboardController < ApplicationController
   private
 
   def chart_colors
-    ["#f8b195", "#f67280", "#c06c84", "#6c5b7b", "#355c7d"]
+    ["#0ad1c4", "#ff6f61", "#fdd456", "#4dd0e1", "#ff8a65", "#d4e157", "#ba68c8", "#4db6ac", "#7986cb", "#a1887f"]
+  end
+
+  def border_color
+    '#111827'
+  end
+
+  def pie_chart_options
+    { donut: true, colors: chart_colors, dataset: { borderColor: border_color, borderWidth: 5 }, legend: false, loading: t('dashboard_controller.loading') }
+  end
+
+  def bar_options
+    { label: t('dashboard_controller.value'), colors: chart_colors, dataset: { borderColor: border_color, borderWidth: 1 }, loading: t('dashboard_controller.loading') }
+  end
+
+  def line_chart_options
+    { label: t('dashboard_controller.value'), curve: false , colors: ['#0ad1c4'], legend: false, loading: t('dashboard_controller.loading') }
   end
 
 end
